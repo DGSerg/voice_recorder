324db2b4576145558af3bf0d20db20379cf1487e
serhii.karpenko <karpenko.serg1992@gmail.com>
2024-12-08 13:08:07 +0200

    Rename file

--- voicerecorder.py
+++ voicerecorder.py
@@ -17,7 +17,6 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 """
-import logging
 from __future__ import print_function
 from sys import byteorder
 from array import array
@@ -27,6 +26,7 @@ import time
 import pyaudio
 import wave
 import os
+import logging
 
 LOGGING_ENABLED = True
 LOG_FILE_PATH = os.path.expanduser("~/vox-recorder.log")
